98a17f651cbf4ad8a1bd23563589c3241e0c4bc2
vasu <vasu@stellentsoft.com>
2014-07-16 16:18:28 +0530

    Initial Commit

---
+++ CocoaPodExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodExample
+//
+//  Created by Stellent Software on 7/16/14.
+//  Copyright (c) 2014 Stellent Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocoaPodExample
+//
+//  Created by Stellent Software on 7/16/14.
+//  Copyright (c) 2014 Stellent Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
